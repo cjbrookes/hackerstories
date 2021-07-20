0ec6c5907c34f34854d350fa7d86df3f52adc0de
Chris Brookes <christopher@dynamox.net>
2021-07-20 09:19:27 -0300

    comments

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const list = [
   },
 ]
 
+//definition of App component
 function App() {
     return (
     <div>
@@ -28,21 +29,28 @@ function App() {
       <input id="search" type="text" />
 
       <hr />
-
-      {list.map(function(item){
-        return (
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span> {item.author} </span>
-          <span> {item.num_comments} </span>
-          <span> {item.points} </span>
-        </div>
-        )
-      })}
+ 
+      <List /> {/*creating an instance of List component*/}
+      <List /> {/*creating another instance of List component*/}
     </div>
   );
+}
+
+//definition of list component
+function List() {
+  return list.map(function(item){
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  });
+
 }
 
 export default App;
